7591668c09ab8a6d07d7f04b6f17564bcae72e1b
Erica Hoi <ericahoi040506@gmail.com>
2024-12-17 11:34:19 +0800

    edit in NewClass

--- NewClass.java
+++ NewClass.java
@@ -1,5 +1,5 @@
 public class NewClass {
     public static void main(String[] args) {
-        System.out.println("Hello from NewClass!");
+        System.out.println("Hello from New2Class!");
     }
 }
